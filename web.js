2f012a81112474b7e0174e3d6f29459a2cd28e6a
Ubuntu <ubuntu@ip-172-31-21-180.us-west-2.compute.internal>
2013-07-08 21:46:52 +0000

    Modified to open and read index.html into a buffer and display buffer contents to screen

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!!');
+	var bufferIn = new Buffer(fs.readFileSync("index.html"));
+	response.send(bufferIn.toString());
 });
 
 var port = process.env.PORT || 5000;
